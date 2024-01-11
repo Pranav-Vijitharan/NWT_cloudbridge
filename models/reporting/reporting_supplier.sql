efa0baa0b6457efa5b6f824740a4dd18338c349b
Blackcatty <johnwongfw@gmail.com>
2024-01-11 13:57:37 +0800

    Camel case for reporting

--- models/reporting/reporting_supplier.sql
+++ models/reporting/reporting_supplier.sql
@@ -1,49 +1,49 @@
 select
 --suppliertable
-    s.supplierid,
-    od.productid,
-    od.orderid,
-    s.companyname as suppliername,
-    contactname,
-    contacttitle,
-    country,
-    s.region,
-    city,
-    address,
-    postalcode,
-    s.phone as supplier_phone,
-    shippeddate,
-    ontimestatus,
+    s.Supplierid,
+    od.Productid,
+    od.Orderid,
+    s.Companyname as Suppliername,
+    Contactname,
+    Contacttitle,
+    Country,
+    s.Region,
+    City,
+    Address,
+    Postalcode,
+    s.Phone as Supplier_phone,
+    Shippeddate,
+    Ontimestatus,
 --shippertable
-    sh.shipperid,
-    sh.companyname as shippername,
-    sh.phone as shipper_phone,
+    sh.Shipperid,
+    sh.Companyname as Shippername,
+    sh.Phone as Shipper_phone,
 --producttable
     sum(totalordervalue) as Product_cost,
     sum(freight) as Shipping_cost
 from {{ ref('stg_supplier') }} as s
 left join {{ ref('stg_products')}} as p
-on s.supplierid=p.supplierid
+on s.Supplierid=p.Supplierid
 left join {{ ref('stg_orderdetails')}} as od
-on p.productid=od.productid
+on p.Productid=od.Productid
 left join {{ref('stg_orders')}} as o
-on o.orderid=od.orderid
+on o.Orderid=od.Orderid
 left join {{ref('stg_shipper')}} as sh
-on o.shipvia=sh.shipperid
-group by s.supplierid,
-    od.productid,
-    od.orderid,
-    s.companyname,
-    contactname,
-    contacttitle,
-    country,
-    s.region,
-    city,
-    address,
-    postalcode,
-    s.phone,
-    sh.shipperid,
-    sh.companyname,
-    sh.phone,
-    shippeddate,
-    ontimestatus
+on o.Shipvia=sh.Shipperid
+group by s.Supplierid,
+    od.Productid,
+    od.Orderid,
+    s.Companyname,
+    Contactname,
+    Contacttitle,
+    Country,
+    s.Region,
+    City,
+    Address,
+    Postalcode,
+    s.Phone,
+    sh.Shipperid,
+    sh.Companyname,
+    sh.Phone,
+    Shippeddate,
+    Ontimestatus
