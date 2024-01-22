cd436a5d62e096b3b590d204166989154d8e98a5
Pranav Vijitharan <pranav.vijitharan@gmail.com>
2024-01-22 15:54:17 +0000

    rename file

---
+++ models/reporting/reporting_employee.sql
@@ -0,0 +1,36 @@
+{{ config (materialized='table')}}
+
+SELECT
+    e.EmployeeID,
+    e.fullName,
+    e.Title,
+    e.age,
+    e.employeeTenure AS EmployeeTenure,
+    e.Address AS EmployeeAddress,
+    e.City AS EmployeeCity,
+    e.Region AS EmployeeRegion,
+    e.PostalCode AS EmployeePostalCode,
+    e.Country AS EmployeeCountry,
+    et.territoryid,
+    e.HomePhone,
+    e.Extension,
+    e.Photo,
+    e.Notes,
+    e.ReportsTo,
+    e.PhotoPath,
+    o.OrderID,
+    o.OrderDate,
+    o.RequiredDate,
+    o.ShippedDate,
+    o.Freight,
+    o.ShipName,
+    o.ShipAddress AS OrderShipAddress,
+    o.ShipCity AS OrderShipCity,
+    o.ShipRegion AS OrderShipRegion,
+    o.ShipPostalCode AS OrderShipPostalCode,
+    o.ShipCountry AS OrderShipCountry
+FROM {{ ref('stg_orders') }} as o
+LEFT JOIN {{ ref('stg_employee') }} as e
+ON e.EmployeeID = o.EmployeeID
+LEFT JOIN {{ ref('stg_employeeterritories') }} as et
+ON e.EmployeeID = et.EmployeeID
