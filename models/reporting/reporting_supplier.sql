6877d2c9b4bcf961c092a4b4163ee378de6d5664
John Wong <s10242004@connect.np.edu.sg>
2024-01-26 09:21:06 +0000

    reporting done

--- models/reporting/reporting_supplier.sql
+++ models/reporting/reporting_supplier.sql
@@ -1,26 +1,42 @@
 select
 --suppliertable
     s.Supplierid,
-    od.Productid,
-    od.Orderid,
     s.Companyname as Suppliername,
     Contactname,
     Contacttitle,
-    Country,
-    s.Region,
-    City,
-    Address,
-    Postalcode,
+    Country as Supplier_country,
+    s.Region as Supplier_Region,
+    City as Supplier_city,
+    Address as Supplier_Address,
+    Postalcode as Supplier_Postalcode,
     s.Phone as Supplier_phone,
-    Shippeddate,
-    Ontimestatus,
+--orderdetails
+    od.Productid,
+    od.Orderid,
+    od.netsales,
+    od.quantity,
+--product
+    p.unitPrice,
+    p.unitCost,
+    p.unitsOnOrder,
+    p.totalOrderValue,
+    p.totalOrderCost,
 --shippertable
     sh.Shipperid,
-    sh.Companyname as Shippername,
+    sh.Companyname as Shippedvia,
     sh.Phone as Shipper_phone,
---producttable
-    sum(totalordervalue) as Product_cost,
-    sum(freight) as Shipping_cost
+--order
+   o.orderDate,
+   o.requiredDate,
+   o.shippedDate,
+   o.shipRegion,
+   o.shipCountry,
+   o.shipCity,
+   o.shipAddress,
+   o.shippingTime,
+   o.shippingDelay,
+   o.onTimeStatus,
+   {{ profit_profitmargin('o', 'od', 'p') }}
 from {{ ref('stg_supplier') }} as s
 left join {{ ref('stg_products')}} as p
 on s.Supplierid=p.Supplierid
@@ -30,20 +46,3 @@ left join {{ref('stg_orders')}} as o
 on o.Orderid=od.Orderid
 left join {{ref('stg_shipper')}} as sh
 on o.Shipvia=sh.Shipperid
-group by s.Supplierid,
-    od.Productid,
-    od.Orderid,
-    s.Companyname,
-    Contactname,
-    Contacttitle,
-    Country,
-    s.Region,
-    City,
-    Address,
-    Postalcode,
-    s.Phone,
-    sh.Shipperid,
-    sh.Companyname,
-    sh.Phone,
-    Shippeddate,
-    Ontimestatus
